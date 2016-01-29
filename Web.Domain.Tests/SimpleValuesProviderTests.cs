376552aae13683c57a4580e1cf52b8cd447c7735
dmitriy.litichevskiy <dmitry.litichevskiy@byndyusoft.com>
2016-01-29 19:35:59 +0500

    Add tests

---
+++ Web.Domain.Tests/SimpleValuesProviderTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Web.Domain.Tests
+{
+    using NUnit.Framework;
+    using ValuesProvider;
+
+    [TestFixture]
+    public class SimpleValuesProviderTests
+    {
+        private readonly SimpleValuesProvider _valuesProvider;
+
+        public SimpleValuesProviderTests()
+        {
+            _valuesProvider = new SimpleValuesProvider();
+        }
+
+        [Test]
+        public void ShouldReturnValueFromConfiguration()
+        {
+            // Given
+            var configiration = new ValuesControllerConfiguration {Value = "aaa", Value1 = "bbb", Value2 = "ccc"};
+
+            // When
+            var v = _valuesProvider.GetValue(configiration);
+
+            // Then
+            Assert.AreEqual(configiration.Value, v);
+        }
+    }
+}
