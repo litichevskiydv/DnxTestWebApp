93f106b70ee511fd3e2950fe2d4602c26123496f
dmitriy.litichevskiy <dmitry.litichevskiy@byndyusoft.com>
2016-01-29 23:28:30 +0500

    Tests execution configurations added

--- src/Web.Domain.Tests/SimpleValuesProviderTests.cs
+++ src/Web.Domain.Tests/SimpleValuesProviderTests.cs
@@ -14,10 +14,23 @@
         }
 
         [Test]
-        public void ShouldReturnValueFromConfiguration()
+        public void ShouldReturnValueFromConfiguration1()
         {
             // Given
             var configiration = new ValuesControllerConfiguration {Value = "aaa", Value1 = "bbb", Value2 = "ccc"};
+
+            // When
+            var v = _valuesProvider.GetValue(configiration);
+
+            // Then
+            Assert.AreEqual(configiration.Value, v);
+        }
+
+        [Test]
+        public void ShouldReturnValueFromConfiguration2()
+        {
+            // Given
+            var configiration = new ValuesControllerConfiguration { Value = "aaa", Value1 = "bbb", Value2 = "ccc" };
 
             // When
             var v = _valuesProvider.GetValue(configiration);
